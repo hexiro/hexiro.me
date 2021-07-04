1e5c1d9cc737a444c65a6b7e190c86b21239aa33
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-04 11:45:48 -0400

    fix: lowercase keys in `index.go`

--- routes/index.go
+++ routes/index.go
@@ -8,7 +8,7 @@ import (
 
 func Index(writer http.ResponseWriter, req *http.Request) {
 	renderer.HTML(writer, 200, "html/index", map[string]interface{}{
-		"Projects": github.Repos,
-		"Config":   config.Config,
+		"projects": github.Repos,
+		"config":   config.Config,
 	})
 }
