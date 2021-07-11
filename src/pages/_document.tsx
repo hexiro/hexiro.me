ec40d954071bcf0237430b9849eea7a3177a61e1
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-11 13:37:10 -0400

    move colors back to css file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import Document, { Head, Html, NextScript, Main } from "next/document";
-import { DarkColors, DarkMainAccent, LightColors } from "../data/config";
 
 export default class extends Document {
     render(): JSX.Element {
         return (
             <Html lang="en">
                 <Head>
-                    <meta name="theme-color" content={DarkMainAccent} />
+                    <meta name="theme-color" content="#5ce09a" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&amp;display=swap"
@@ -16,8 +15,6 @@ export default class extends Document {
                         name="keywords"
                         content="Hexiro, Hex, Hexiiro, Programmer, Software Engineer, Python"
                     />
-                    {DarkColors}
-                    {LightColors}
                 </Head>
                 <body>
                     <Main></Main>
