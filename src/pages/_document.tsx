e20d41f5a0be26930c08450c7a3d9a059731063a
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-11 18:59:56 -0400

    add + setup favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,15 +6,21 @@ export default class extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="theme-color" content="#5ce09a" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&amp;display=swap"
-                        rel="stylesheet"
-                    />
+                    <meta name="msapplication-TileColor" content="#191c1d" />
                     <meta
                         name="keywords"
                         content="Hexiro, Hex, Hexiiro, Programmer, Software Engineer, Python"
                     />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&amp;display=swap"
+                        rel="stylesheet"
+                    />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5ce09a" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
                 </Head>
                 <body>
                     <Main></Main>
