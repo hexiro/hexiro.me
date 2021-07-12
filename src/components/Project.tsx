24c89eca9b93fc219e1d9d825c0ffa7cf59e7c38
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-11 21:29:48 -0400

    add component, `Project`

---
+++ src/components/Project.tsx
@@ -0,0 +1,90 @@
+import { BiGitCommit, BiGitPullRequest, BiGitRepoForked, BiStar } from "react-icons/bi";
+
+import { Github } from "../data/config";
+import parse from "html-react-parser";
+
+export interface ProjectProps {
+    name: string;
+    descriptionHTML: string;
+    url: string;
+    owner: {
+        login: string;
+    };
+    stargazers: {
+        totalCount: number;
+    };
+    forks: {
+        totalCount: number;
+    };
+    pullRequests: {
+        totalCount: number;
+    };
+    issues: {
+        totalCount: number;
+    };
+    primaryLanguage: {
+        name: string;
+    };
+    defaultBranchRef: {
+        target: {
+            history: {
+                totalCount: number;
+            };
+        };
+    };
+}
+
+export default function Project(project: ProjectProps): JSX.Element {
+    return (
+        <div className="projects-item transition">
+            <div className="projects-container">
+                <div className="project-title main-color">
+                    <a href={project.url} rel="noreferrer" target="_blank">
+                        {project.owner.login == Github ? (
+                            <h3>{project.name}</h3>
+                        ) : (
+                            <h3>
+                                <span className="main-accent">{project.owner.login}/</span>
+                                {project.name}
+                            </h3>
+                        )}
+                    </a>
+                </div>
+                <p>
+                    {
+                        // input is parser safe and will never include a script tag
+                        // remove redundant <div></div> with .slice(5, -6)
+                        parse(project.descriptionHTML.slice(5, -6))
+                    }
+                </p>
+                <div className="projects-footer">
+                    <span className="project-language">{project.primaryLanguage.name}</span>
+                    {project.stargazers.totalCount > 0 && (
+                        <div className="project-detail">
+                            <BiStar />
+                            <h4>{project.stargazers.totalCount}</h4>
+                        </div>
+                    )}
+                    {project.forks.totalCount > 0 && (
+                        <div className="project-detail">
+                            <BiGitRepoForked />
+                            <h4>{project.forks.totalCount}</h4>
+                        </div>
+                    )}
+                    {project.pullRequests.totalCount > 0 && (
+                        <div className="project-detail">
+                            <BiGitPullRequest />
+                            <h4>{project.pullRequests.totalCount}</h4>
+                        </div>
+                    )}
+                    {
+                        <div className="project-detail">
+                            <BiGitCommit />
+                            <h4>{project.defaultBranchRef.target.history.totalCount}</h4>
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,12 @@
-import Head from "next/head";
+import { Age, Github, GithubLink, GithubToken, Twitter, TwitterLink } from "../data/config";
+import { FiGithub, FiTwitter } from "react-icons/fi";
+import Project, { ProjectProps } from "../components/Project";
+
 import Link from "next/link";
 import PageHeading from "../components/Heading";
-import { Age, Github, GithubLink, GithubToken, Twitter, TwitterLink } from "../data/config";
-import { FiTwitter, FiGithub } from "react-icons/fi";
-import { BiStar, BiGitCommit, BiGitPullRequest, BiGitRepoForked } from "react-icons/bi";
-import parse from "html-react-parser";
-
-interface Project {
-    name: string;
-    descriptionHTML: string;
-    url: string;
-    owner: {
-        login: string;
-    };
-    stargazers: {
-        totalCount: number;
-    };
-    forks: {
-        totalCount: number;
-    };
-    pullRequests: {
-        totalCount: number;
-    };
-    issues: {
-        totalCount: number;
-    };
-    primaryLanguage: {
-        name: string;
-    };
-    defaultBranchRef: {
-        target: {
-            history: {
-                totalCount: number;
-            };
-        };
-    };
-}
 
 interface HomeProps {
-    projects: Project[];
+    projects: ProjectProps[];
     age: number;
     github: string;
     githubLink: string;
@@ -99,7 +67,7 @@ export const getStaticProps = async () => {
         body: JSON.stringify(data),
     });
     const json = await res.json();
-    const projects: Project[] = json["data"]["user"]["pinnedItems"]["nodes"];
+    const projects: ProjectProps[] = json["data"]["user"]["pinnedItems"]["nodes"];
 
     return {
         props: {
@@ -118,7 +86,7 @@ export default function Home({ projects, age, github, githubLink, twitterLink }:
     const description = `A ${age} y/o aspiring Software Engineer`;
     return (
         <>
-            <PageHeading pageName="Home" description={description}/>
+            <PageHeading pageName="Home" description={description} />
             <main>
                 <div className="left">
                     <div className="intro">
@@ -130,66 +98,19 @@ export default function Home({ projects, age, github, githubLink, twitterLink }:
                 </div>
                 <div className="right">
                     <div className="projects">
-                        {projects.map((project: Project) => (
-                            <div className="projects-item transition">
-                                <div className="projects-container">
-                                    <div className="project-title main-color">
-                                        <a href={project.url} rel="noreferrer" target="_blank">
-                                            {project.owner.login == github ? (
-                                                <h3>{project.name}</h3>
-                                            ) : (
-                                                <h3>
-                                                    <span className="main-accent">
-                                                        {project.owner.login}/
-                                                    </span>
-                                                    {project.name}
-                                                </h3>
-                                            )}
-                                        </a>
-                                    </div>
-                                    <p>
-                                        {
-                                            // input is parser safe and will never include a script tag
-                                            // remove redundant <div></div> with .slice(5, -6)
-                                            parse(project.descriptionHTML.slice(5, -6))
-                                        }
-                                    </p>
-                                    <div className="projects-footer">
-                                        <span className="project-language">
-                                            {project.primaryLanguage.name}
-                                        </span>
-                                        {project.stargazers.totalCount > 0 && (
-                                            <div className="project-detail">
-                                                <BiStar />
-                                                <h4>{project.stargazers.totalCount}</h4>
-                                            </div>
-                                        )}
-                                        {project.forks.totalCount > 0 && (
-                                            <div className="project-detail">
-                                                <BiGitRepoForked />
-                                                <h4>{project.forks.totalCount}</h4>
-                                            </div>
-                                        )}
-                                        {project.pullRequests.totalCount > 0 && (
-                                            <div className="project-detail">
-                                                <BiGitPullRequest />
-                                                <h4>{project.pullRequests.totalCount}</h4>
-                                            </div>
-                                        )}
-                                        {
-                                            <div className="project-detail">
-                                                <BiGitCommit />
-                                                <h4>
-                                                    {
-                                                        project.defaultBranchRef.target.history
-                                                            .totalCount
-                                                    }
-                                                </h4>
-                                            </div>
-                                        }
-                                    </div>
-                                </div>
-                            </div>
+                        {projects.map((project) => (
+                            <Project
+                                name={project.name}
+                                descriptionHTML={project.descriptionHTML}
+                                url={project.url}
+                                owner={project.owner}
+                                stargazers={project.stargazers}
+                                forks={project.forks}
+                                pullRequests={project.pullRequests}
+                                issues={project.issues}
+                                primaryLanguage={project.primaryLanguage}
+                                defaultBranchRef={project.defaultBranchRef}
+                            />
                         ))}
                     </div>
                 </div>
