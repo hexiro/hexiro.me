866c1d1e9288a2805bf6348d11f6ad7c5c7c8c57
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-26 15:42:30 -0400

    chore: `Timestamp.tsx` code cleanup

--- src/components/Timestamp.tsx
+++ src/components/Timestamp.tsx
@@ -6,34 +6,18 @@ const relativeTime = (stamp: number): number => {
     return Math.floor((Date.now() - stamp) / 1000);
 };
 
-const songDuration = (stamp: number): string => {
-    const quotient = Math.floor(stamp / 60);
-    const remainder = stamp % 60;
-    return `${quotient}:${String(remainder).padStart(2, "0")}`;
-};
-
-const formatElapsed = (start: number): JSX.Element => {
-    const rel = relativeTime(start);
-    let hours = Math.floor(rel / 3600);
-    let minutes = Math.floor(rel / 60) % 60;
-    let seconds = rel % 60;
-    let formatted = [hours, minutes, seconds]
-        .map((v) => ("" + v).padStart(2, "0"))
-        .filter((v, i) => v !== "00" || i > 0)
-        .join(":");
-    return <h5>{`${formatted} elapsed`}</h5>;
-};
-
 const formatSong = (start: number, end: number): JSX.Element => {
     let relStart = relativeTime(start);
     const relEnd = Math.floor((end - start) / 1000);
     if (relStart > relEnd) {
         relStart = relEnd;
     }
-    const width = relStart / relEnd;
     return (
         <div className="song-outer-bar">
-            <div className="song-inner-bar" style={{ width: `${width * 100}%` }}></div>
+            <div
+                className="song-inner-bar"
+                style={{ width: `${(relStart / relEnd) * 100}%` }}
+            ></div>
         </div>
     );
 };
