ecf4317df6163d3e57a1fa157aa8e9547a1894ce
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-05 15:59:37 -0400

    feat: setup styled-components in `_document`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+
+import { ServerStyleSheet } from "styled-components";
 
 export default class extends Document {
     render(): JSX.Element {
@@ -35,5 +37,32 @@ export default class extends Document {
                 </body>
             </Html>
         );
+    }
+
+    // copied from
+    // https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js#L4
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 }
