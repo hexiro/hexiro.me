da3b6633afcb1baac9b943f65d813869ed2135f6
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-05 18:47:42 -0400

    refactor: index w/ styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,60 +2,77 @@ import React from "react";
 
 import { HomeProps, ProjectProps } from "types";
 
-import { FadeIn, Lanyard, Project, SocialMedia } from "components";
+import { FadeIn, Lanyard, Project, Socials } from "components";
 import Page from "components/pages";
-import { Age, Github, GithubLink, SteamLink, TwitterLink } from "data/config";
+import { Age, Github } from "data/config";
 import GraphQL from "data/graphql";
+import theme from "data/theme";
 
-import { RiGithubLine, RiSteamLine, RiTwitterLine } from "react-icons/ri";
+import styled, { css } from "styled-components";
 
 export default function Home({ projects }: HomeProps): JSX.Element {
     const description = `A ${Age()} y/o aspiring Software Engineer`;
     return (
-        <Page name="Home" description={description}>
-            <main>
-                <div className="left">
-                    <FadeIn>
-                        <div className="intro">
-                            <h1>
-                                Hi! I'm <span className="main-accent font-weight-400">Hexiro</span>,
-                            </h1>
-                            <h2>{description}</h2>
-                            <FadeIn
-                                className="socials-items"
-                                style={{ display: "unset" }}
-                                delay={120}
-                                transitionDuration={450}
-                            >
-                                <ul className="socials">
-                                    <SocialMedia href={TwitterLink}>
-                                        <RiTwitterLine />
-                                    </SocialMedia>
-                                    <SocialMedia href={GithubLink}>
-                                        <RiGithubLine />
-                                    </SocialMedia>
-                                    <SocialMedia href={SteamLink}>
-                                        <RiSteamLine />
-                                    </SocialMedia>
-                                </ul>
-                            </FadeIn>
-                            <Lanyard />
-                        </div>
-                    </FadeIn>
-                </div>
-                <div className="right">
-                    <FadeIn delay={80} transitionDuration={425}>
-                        <div className="projects">
-                            {projects.map(project => (
-                                <Project {...project} />
-                            ))}
-                        </div>
-                    </FadeIn>
-                </div>
-            </main>
+        <Page name="Home" description={description} locked={true}>
+            <Main>
+                <Side side="left">
+                    <Intro>
+                        <h1>
+                            Hi! I'm <Name>Hexiro</Name>,
+                        </h1>
+                        <h2>{description}</h2>
+                        <Socials />
+                        <Lanyard />
+                    </Intro>
+                </Side>
+                <Side side="right">
+                    <div className="projects">
+                        {projects.map(project => (
+                            <Project {...project} />
+                        ))}
+                    </div>
+                </Side>
+            </Main>
         </Page>
     );
 }
+
+const Main = styled.main`
+    display: flex;
+    align-content: center;
+    min-height: 100vh;
+    height: 100%;
+`;
+
+// prettier-ignore
+const Side = styled.div<{ side: "left" | "right" }>`
+    display: flex;
+    flex: 1;
+    align-items: center;
+    ${({ side }) => side === "right" && css`
+            justify-content: center;
+            margin: 20px 0;
+      `}
+`;
+
+// transform makes it so the vertical centering is centered around the description line
+// instead of around the whole div
+const Intro = styled.div`
+    line-height: 3em;
+    margin-left: 30px;
+    min-height: 115px;
+    position: relative;
+    transform: translateY(-20%);
+
+    & ul {
+        margin-top: 10px;
+    }
+`;
+
+const Name = styled.span`
+    color: ${theme.accent.main};
+    font-weight: 400;
+`;
 
 // regen top 3 pinned repos every hour
 export const getStaticProps = async () => {
