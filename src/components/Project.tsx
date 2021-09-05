9451c23c56ed04793ed28ec36fbe0cd691ccd008
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-05 19:07:31 -0400

    refactor: covert `Project` to styled-components

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,14 +2,16 @@ import { ProjectProps } from "types";
 
 import { ParseHTML } from "components";
 import { Github } from "data/config";
+import theme from "data/theme";
 
 import { BiGitCommit, BiGitPullRequest, BiGitRepoForked, BiStar } from "react-icons/bi";
+import styled from "styled-components";
 
 export const Project = (project: ProjectProps): JSX.Element => {
     return (
-        <div className="projects-item">
-            <div className="projects-container">
-                <div className="project-title main-color">
+        <ProjectsItem>
+            <ProjectsContainer>
+                <ProjectsTitle>
                     <h3>
                         <a href={project.url} rel="noreferrer" target="_blank">
                             {project.owner.login == Github ? (
@@ -22,40 +24,101 @@ export const Project = (project: ProjectProps): JSX.Element => {
                             )}
                         </a>
                     </h3>
-                </div>
+                </ProjectsTitle>
                 <p>
                     <ParseHTML html={project.descriptionHTML.slice(5, -6)} />
                 </p>
-                <div className="projects-footer">
-                    <span className="project-language">{project.primaryLanguage.name}</span>
+                <ProjectsFooter>
+                    <ProjectLanguage>{project.primaryLanguage.name}</ProjectLanguage>
                     <ul>
                         {project.stargazers.totalCount > 0 && (
-                            <li className="project-detail">
+                            <ProjectDetail>
                                 <BiStar />
                                 <h4>{project.stargazers.totalCount}</h4>
-                            </li>
+                            </ProjectDetail>
                         )}
                         {project.forks.totalCount > 0 && (
-                            <li className="project-detail">
+                            <ProjectDetail>
                                 <BiGitRepoForked />
                                 <h4>{project.forks.totalCount}</h4>
-                            </li>
+                            </ProjectDetail>
                         )}
                         {project.pullRequests.totalCount > 0 && (
-                            <li className="project-detail">
+                            <ProjectDetail>
                                 <BiGitPullRequest />
                                 <h4>{project.pullRequests.totalCount}</h4>
-                            </li>
+                            </ProjectDetail>
                         )}
                         {
-                            <li className="project-detail">
+                            <ProjectDetail>
                                 <BiGitCommit />
                                 <h4>{project.defaultBranchRef.target.history.totalCount}</h4>
-                            </li>
+                            </ProjectDetail>
                         }
                     </ul>
-                </div>
-            </div>
-        </div>
+                </ProjectsFooter>
+            </ProjectsContainer>
+        </ProjectsItem>
     );
 };
+
+const ProjectsTitle = styled.div`
+    padding-top: 10px;
+    word-break: break-word;
+    color: ${theme.core.main};
+
+    & h3 a {
+        opacity: 1;
+    }
+`;
+
+const ProjectsItem = styled.div`
+    position: relative;
+    min-width: 530px;
+    max-width: 630px;
+    height: 220px;
+    border-radius: 10px;
+    background: ${theme.accent.background};
+    padding: 4px 20px;
+    margin: 25px 0;
+    box-shadow: 0 6px 13px rgba(0, 0, 0, 0.25);
+    transition: ease-out all 0.31s;
+    will-change: transform;
+
+    &:hover {
+        transform: scale(1.04);
+    }
+`;
+
+const ProjectsContainer = styled.div`
+    & p {
+        padding-bottom: 10px;
+    }
+`;
+
+const ProjectsFooter = styled.div`
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 6px;
+
+    & h4 {
+        margin-left: 2px;
+        line-height: 33px;
+    }
+
+    & svg {
+        height: 18px;
+        width: 18px;
+    }
+`;
+
+const ProjectLanguage = styled.span`
+    margin-right: 20px;
+`;
+
+const ProjectDetail = styled.li`
+    display: inline-flex;
+    align-items: center;
+    margin-right: 10px;
+`;
