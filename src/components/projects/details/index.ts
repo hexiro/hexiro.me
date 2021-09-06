c9710386e17bb3a8d6c6299b189c778e78b972b3
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-06 15:23:01 -0400

    fix: maybe?
    idk man this works on my machine :+1: :+1: :+1:

--- src/components/projects/details/index.ts
+++ src/components/projects/details/index.ts
@@ -1,4 +1,6 @@
-export * from "components/projects/details/Commits";
-export * from "components/projects/details/Forks";
-export * from "components/projects/details/PullRequests";
-export * from "components/projects/details/Stars";
+import { Commits } from "components/projects/details/Commits";
+import { Forks } from "components/projects/details/Forks";
+import { PullRequests } from "components/projects/details/PullRequests";
+import { Stars } from "components/projects/details/Stars";
+
+export { Commits, Forks, PullRequests, Stars };
