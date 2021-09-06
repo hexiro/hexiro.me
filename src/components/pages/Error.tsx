365748d2497a3193355ecc06e425e338e592cb1b
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-05 20:51:47 -0400

    fix: `ErrorPage`

--- src/components/pages/Error.tsx
+++ src/components/pages/Error.tsx
@@ -1,4 +1,7 @@
+import { Header } from "components";
 import Page from "components/Pages";
+
+import styled from "styled-components";
 
 export const ErrorPage = ({
     status,
@@ -9,12 +12,25 @@ export const ErrorPage = ({
 }): JSX.Element => {
     return (
         <Page name={String(status)} description={message || "Oops?"} locked={true}>
-            <main className="full-center">
-                <div className="text-center">
-                    <h1 className="main-accent font-weight-400">{status}</h1>
-                    <h2 className="font-weight-400">{message}</h2>
-                </div>
-            </main>
+            <Main>
+                <Text>
+                    <h1>
+                        <Header>{status}</Header>
+                    </h1>
+                    <h2>{message}</h2>
+                </Text>
+            </Main>
         </Page>
     );
 };
+
+const Main = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const Text = styled.div`
+    text-align: center;
+`;
