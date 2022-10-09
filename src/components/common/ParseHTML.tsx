b02d1f8b6a3ae142d8b719b10233880444c185ce
Cody Miller <hello@looskie.com>
2022-10-09 12:37:54 -0400

    ignore

--- src/components/common/ParseHTML.tsx
+++ src/components/common/ParseHTML.tsx
@@ -13,6 +13,7 @@ const replace = (node: DOMNode): JSX.Element | null => {
     const key = hash(node);
 
     if (node instanceof Text) {
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
         if (node.next || node.prev) {
             return <span key={key}>{node.data}</span>;
         }
