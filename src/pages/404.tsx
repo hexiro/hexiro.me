b65effa83e25c862d46c3cc6b781f5efa6fe8b56
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-02-08 17:53:02 -0500

    better python, ts, js icons

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,4 @@
-import { styled } from "@/theme";
-
-import { Button, ButtonLink, Heading, Link, Paragraph } from "@/components/ui";
+import { ButtonLink } from "@/components/ui";
 
 import Page from "@/layout/Page";
 
@@ -10,21 +8,9 @@ const DESCRIPTION = "404 | Not Found";
 export default function NotFound() {
     return (
         <Page name={NAME} description={DESCRIPTION} css={{ paddingTop: 240 }}>
-            {/* <Heading as="h1" css={{ fontSize: 96 }}>
-                404
-            </Heading>
-            <Paragraph size="lg">{"Oops? This page couldn't be found!"}</Paragraph> */}
             <ButtonLink priority="secondary" href="/" css={{ marginTop: "2em" }}>
                 Go Home
             </ButtonLink>
         </Page>
     );
 }
-
-// const ButtonLinkWrapper = styled(Button, {
-//     color: "inherit",
-//     textDecoration: "inherit",
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-// });
