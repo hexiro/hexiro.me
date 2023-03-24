dd2770aff9a4627209e6114bbdf9dc226c2ef16e
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-03-24 18:16:37 -0400

    z-index fix

--- src/components/ui/LinkOverlay.tsx
+++ src/components/ui/LinkOverlay.tsx
@@ -12,7 +12,7 @@ const LinkOverlay = styled(Link, {
         position: "absolute",
         top: 0,
         left: 0,
-        zIndex: 0,
+        zIndex: 1,
         width: "100%",
         height: "100%",
     },
