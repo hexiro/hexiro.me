bd13b92e3b185db3c2b89d962dcea3ab8fc70d98
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-07-04 16:56:27 -0400

    copy contact button

---
+++ src/components/contact/SocialCard.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import type { ISocial } from "@/commons/config";
+
+import { ExternalLinkOverlay } from "@/components/layout/LinkOverlay";
+import { Card } from "@/components/ui/Cards";
+import { ExternalLink } from "@/components/ui/ExternalLink";
+import { H5 } from "@/components/ui/Headings";
+import { CheckIcon, CopyIcon, ExternalLinkIcon, XIcon } from "@/components/ui/Icons";
+
+export function SocialCard({ social }: { social: ISocial }) {
+    const { name, value, link, icon: Icon } = social;
+    const [copiedSuccess, setCopiedSuccess] = useState<boolean | null>(null);
+    const [copiedTimeout, setCopiedTimeout] = useState<NodeJS.Timeout | null>(null);
+
+    const copyToClipboard = async () => {
+        if (copiedTimeout) clearTimeout(copiedTimeout);
+
+        try {
+            await navigator.clipboard.writeText(value);
+            setCopiedSuccess(true);
+        } catch (err) {
+            setCopiedSuccess(false);
+        } finally {
+            const clear = () => {
+                setCopiedSuccess(null);
+                setCopiedTimeout(null);
+            };
+
+            const timeout = setTimeout(clear, 3000);
+            setCopiedTimeout(timeout);
+        }
+    };
+
+    return (
+        <Card as="li" className="flex flex-row items-center gap-x-6 min-w-[375px]">
+            <Icon className="w-10 h-10" />
+            <div className="flex flex-col">
+                <H5 className="text-off-white font-bold text-[20px]">{name}</H5>
+                <ExternalLinkOverlay href={link} className="font-mono font-bold text-text">
+                    {value}
+                </ExternalLinkOverlay>
+            </div>
+            <div className="absolute top-6 right-8 flex flex-row gap-x-2">
+                <button
+                    type="button"
+                    className="text-text font-bold font-mono"
+                    onClick={copyToClipboard}
+                >
+                    {copiedSuccess === null ? (
+                        <CopyIcon />
+                    ) : copiedSuccess ? (
+                        <CheckIcon />
+                    ) : (
+                        <XIcon className="text-red-400" />
+                    )}
+                </button>
+                <ExternalLink href={link}>
+                    <ExternalLinkIcon />
+                </ExternalLink>
+            </div>
+        </Card>
+    );
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,8 @@
 import { SOCIALS } from "@/commons/config";
 
-import { ExternalLinkOverlay } from "@/components/layout/LinkOverlay";
-import { Card } from "@/components/ui/Cards";
-import { ExternalLink } from "@/components/ui/ExternalLink";
-import { H1, H3, H5 } from "@/components/ui/Headings";
-import { ExternalLinkIcon } from "@/components/ui/Icons";
+import { H1, H3 } from "@/components/ui/Headings";
+
+import { SocialCard } from "@/components/contact/SocialCard";
 
 export default function ContactPage() {
     return (
@@ -14,28 +12,8 @@ export default function ContactPage() {
                 <H3 className="text-subtitle">Connect w/ me through another medium</H3>
             </div>
             <ul className="flex flex-row flex-wrap gap-6">
-                {SOCIALS.map(({ name, value, link, icon: Icon }) => (
-                    <Card
-                        key={name}
-                        as="li"
-                        className="flex flex-row items-center gap-x-6 min-w-[375px]"
-                    >
-                        <Icon className="w-10 h-10" />
-                        <div className="flex flex-col">
-                            <H5 className="text-off-white font-bold text-[20px]">{name}</H5>
-                            <ExternalLinkOverlay
-                                href={link}
-                                className="font-mono font-bold text-text"
-                            >
-                                {value}
-                            </ExternalLinkOverlay>
-                        </div>
-                        <div className="absolute top-6 right-8">
-                            <ExternalLink href={link}>
-                                <ExternalLinkIcon />
-                            </ExternalLink>
-                        </div>
-                    </Card>
+                {SOCIALS.map((social) => (
+                    <SocialCard key={social.name} social={social} />
                 ))}
             </ul>
         </>
