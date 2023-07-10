f78117cbab4950509429c73c1b3f6d736b0a7ce0
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-07-10 19:19:27 -0400

    remove lenis

--- src/layout/Page.tsx
+++ src/layout/Page.tsx
@@ -1,20 +1,17 @@
 import { memo } from "react";
 
-import { Lenis } from "@studio-freight/react-lenis";
 import { twMerge } from "tailwind-merge";
 
 function Page({ cssVariables, children }: { cssVariables: string[]; children: React.ReactNode }) {
     return (
-        <Lenis root>
-            <div
-                className={twMerge(
-                    "min-w-screen relative flex h-full min-h-screen w-full flex-col overflow-y-auto overflow-x-hidden bg-background md:flex-row",
-                    ...cssVariables
-                )}
-            >
-                {children}
-            </div>
-        </Lenis>
+        <div
+            className={twMerge(
+                "min-w-screen relative flex h-full min-h-screen w-full flex-col overflow-y-auto overflow-x-hidden bg-background md:flex-row",
+                ...cssVariables
+            )}
+        >
+            {children}
+        </div>
     );
 }
 
