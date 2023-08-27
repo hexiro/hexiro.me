006da2611f45bed5986f127b6800f0f00a7513ff
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-08-27 15:04:41 -0400

    improve skill card styling

--- src/components/cards/SkillCard.tsx
+++ src/components/cards/SkillCard.tsx
@@ -20,17 +20,22 @@ export default function SkillCard({ skill, className }: ISocialCardProps) {
         <Card
             isHoverable
             as="li"
-            className={twMerge("flex w-full flex-row items-center gap-x-6 px-6 md:px-8", className)}
+            className={twMerge(
+                "flex w-full flex-row justify-between gap-x-1 px-[8%] md:px-8",
+                className
+            )}
         >
-            <Icon className="h-10 w-10 shrink-0" />
-            <H5 className="text-[20px] font-bold text-off-white">
-                <ExternalLinkOverlay href={link}>{name}</ExternalLinkOverlay>
-            </H5>
-            <div className="absolute right-8 top-6 hidden flex-row gap-x-2 xxs:flex">
+            <span className="flex h-full grow flex-row items-center gap-x-[8%]">
+                <Icon className="h-8 w-8 shrink-0 xs:h-10 xs:w-10" />
+                <H5 className="truncate text-[20px] font-bold text-off-white">
+                    <ExternalLinkOverlay href={link}>{name}</ExternalLinkOverlay>
+                </H5>
+            </span>
+            <span className="hidden h-full flex-row items-start gap-x-2 xxs:flex">
                 <ExternalLink href={link} className="transition-transform hover:translate-y-[-2px]">
-                    <ExternalLinkIcon />
+                    <ExternalLinkIcon className="h-5 w-5 shrink-0 md:h-6 md:w-6" />
                 </ExternalLink>
-            </div>
+            </span>
         </Card>
     );
 }
