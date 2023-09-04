4d8b0166c6b7f250e625e347ad2b48b6d7a63a7b
nathan lodge <42787085+hexiro@users.noreply.github.com>
2023-09-04 19:21:08 -0400

    update text on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import type { ComponentProps, PropsWithChildren } from "react";
 
-import { AGE, SOCIALS_MAP } from "@/commons/config";
+import { currentAge, SOCIALS_MAP } from "@/commons/config";
 
 import { Card } from "@/components/ui/Cards";
 import { H1, H3, H4 } from "@/components/ui/Headings";
@@ -16,13 +16,14 @@ import { twMerge } from "tailwind-merge";
 const NAME = "About";
 const DESCRIPTION = "My life beyond the IDE.";
 
-const INTRO = `Hey, I'm Nathan Lodge :p. I'm an ${AGE}-year-old software engineer based in the United States. Thank you for checking out my website :), I've been a passionate programmer since being 13 years old in middle school, and since then I've developed my skills and created a catalog of projects. Instead of repeating myself, I'm here to tell you about my life outside of software development.`;
+const INTRO = `Hey, I'm Nathan Lodge! An ${currentAge()}-year-old software engineer based in the United States. Thanks for checking out my website :), I've been a passionate programmer for over 5 years now, primarily specializing in Python and TypeScript. If you'd like to learn more about my technical skills, please feel free to check out the rest of my website and connect with me! With that being said, I'm here to tell you a little more about my life beyond the code editor.
+`;
 
 const MAIN_HOBBIES =
-    "While programming is my primary hobby, I of course do other human things like hanging out with my friends, watching movies, and listening to music. My favorite genres of movies are psychological thrillers, drama, and crime. I also, naturally, love listening to music. My favorite genres are rap and r&b, and to be a little more specific, I like plugg, pluggnb, cloud rap, and trap.";
+    "For fun, I like to hang out with my friends, watch movies and TV shows, listen to music, and play video games. My favorite movie genres are psychological thrillers, drama, and crime, but I'm a big fan of all kinds of films, new and old. I also like listening to music, primarily chill modern rap and r&b.";
 
 const VIDEO_GAMES =
-    "My last primary hobby is playing video games :o. My main game for the past 5 or so years has been Counter-Strike: Global Offensive, which I play when I'm feeling competitive. I'm Master Guaridan 2 as of writing this, which is a little above average so I'm pretty solid. Minecraft is another game that I play, usually just single-player when I'm just trying to chill. When trying to play with friends Fortnite is a game that we usually lean towards. I'm also a fan of story-style games, but I haven't explored the genre too much (open to recommendations). The main ones I've enjoyed are GTA 5, GTA 4, GTA San Andreas, Saints Row 2, and a few Call of Duty campaigns.";
+    "As for the video games I play, I enjoy the first-person shooter genre. My favorite and most played video game is Counter-Strike: Global Offensive, which I play when I'm feeling competitive. My competitive rank is Distinguished Master Guardian as of writing this, which is rank number 14/18, so I'm pretty solid. When I'm not feeling competitive, you can find me playing Minecraft, Satisfactory, Terraria, and other single-player games. I've been exploring some story-style games as of late and have enjoyed playing GTA 5, GTA 4, GTA San Andreas, Saints Row 2, and a few Call of Duty campaigns.";
 
 export default function AboutPage() {
     return (
@@ -40,12 +41,12 @@ export default function AboutPage() {
                         src={introSrc}
                     />
                 </AboutSection>
-                <AboutSection text={MAIN_HOBBIES} childrenClassName="flex flex-col gap-y-4">
+                <AboutSection text={MAIN_HOBBIES}>
                     <ContactCard isSingle social={SOCIALS_MAP.IMDb} />
                     <ContactCard isSingle social={SOCIALS_MAP.Spotify} />
                     <ContactCard isSingle social={SOCIALS_MAP["Last.fm"]} />
                 </AboutSection>
-                <AboutSection text={VIDEO_GAMES} childrenClassName="flex flex-col gap-y-4">
+                <AboutSection text={VIDEO_GAMES}>
                     <ContactCard isSingle social={SOCIALS_MAP.Steam} />
                     <ContactCard isSingle social={SOCIALS_MAP["Epic Games"]} />
                 </AboutSection>
@@ -54,13 +55,12 @@ export default function AboutPage() {
     );
 }
 
-interface IAboutSectionProps extends PropsWithChildren {
+interface IAboutSectionProps {
     text: string;
     className?: string;
-    childrenClassName?: string;
 }
 
-const AboutSection = ({ text, className, children, childrenClassName }: IAboutSectionProps) => (
+const AboutSection = ({ text, className, children }: PropsWithChildren<IAboutSectionProps>) => (
     <div
         className={twMerge(
             "flex flex-col items-center gap-x-[5%] gap-y-6 xl:flex-row xl:even:flex-row-reverse",
@@ -68,7 +68,7 @@ const AboutSection = ({ text, className, children, childrenClassName }: IAboutSe
         )}
     >
         <p className="text-[18px] font-bold leading-relaxed">{text}</p>
-        <div className={twMerge("w-full", childrenClassName)}>{children}</div>
+        <div className="flex w-full flex-col gap-y-4">{children}</div>
     </div>
 );
 
