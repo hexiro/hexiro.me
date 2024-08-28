67a533e19de895596fe71e3af9566a4ba2ced41f
nathan lodge <42787085+hexiro@users.noreply.github.com>
2024-08-27 21:28:51 -0400

    ui/ux: add `role='heading'` to large spans

--- src/components/cards/DiscordCard.tsx
+++ src/components/cards/DiscordCard.tsx
@@ -64,7 +64,10 @@ export function DiscordCard({ className }: { readonly className?: string }) {
                         )}
                     </div>
                     <div className="my-1 w-full overflow-hidden">
-                        <span className="mb-2 truncate font-sans text-[18px] font-extrabold uppercase leading-none text-off-white sm:leading-normal lg:text-base">
+                        <span
+                            role="heading"
+                            className="mb-2 truncate font-sans text-[18px] font-extrabold uppercase leading-none text-off-white sm:leading-normal lg:text-base"
+                        >
                             {state.ide ? state.ide.name : "No IDE active"}
                         </span>
                         {state.ide ? (
